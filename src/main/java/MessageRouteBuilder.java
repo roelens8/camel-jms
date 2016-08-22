5e277330c992f73e4e4e53508511aa60b053e8ea
Armand Roelens <armand.roelens@connexta.com>
2016-08-22 12:49:42 -0700

    Add camel-jms project

---
+++ src/main/java/MessageRouteBuilder.java
@@ -0,0 +1,40 @@
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.amqp.AMQPComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class MessageRouteBuilder extends RouteBuilder {
+
+    Logger LOGGER = LoggerFactory.getLogger(MessageRouteBuilder.class);
+
+    public void configure() {
+
+        Map<String, Object> message = createMessage();
+        try {
+            AMQPComponent amqp = AMQPComponent.amqp10Component("amqp://admin:admin@localhost:5672");
+            getContext().addComponent("amqp", amqp);
+
+            from("timer:simple?period=10000").setBody(constant(message))
+                    .to("amqp:jms.topic.message");
+            from("amqp:jms.topic.message").convertBodyTo(String.class)
+                    .to("stream:out");
+        } catch (MalformedURLException e) {
+            LOGGER.error("Could not create AMQP component.", e);
+        }
+    }
+
+    private static Map<String, Object> createMessage() {
+        Map<String, Object> sparseMessage = new HashMap<>();
+        sparseMessage.put("File_Name", "file1.txt");
+        sparseMessage.put("ID", "1234");
+        return sparseMessage;
+    }
+
+}
